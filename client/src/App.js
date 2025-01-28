ca4e67767cabfd56990b1b88b3af556488d6d91d
Oleg <kaspruk.oleg2002@gmail.com>
2025-01-27 22:35:32 -0600

    Worked with redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,46 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Landing from './components/layout/Landing';
 import Navbar from './components/layout/Navbar';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Alert  from './components/layout/Alert';
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
 
 import './App.css';
 
-const App = () => (
-  <Router>
-    <Fragment>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Landing />} />
-      </Routes>
-      <section className="container">
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
+
+const App = () => {
+  useEffect(()=>{
+    store.dispatch(loadUser());
+  },[])
+
+
+  return (
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
         <Routes>
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Landing />} />
         </Routes>
-      </section>
-    </Fragment>
-  </Router>
-);
+        <section className="container">
+          <Alert/>
+          <Routes>
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </section>
+      </Fragment>
+    </Router>
+  </Provider>
+)};
 
 export default App;
