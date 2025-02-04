9561016bed973594f2c4e0f4cef20cd2c636676d
Oleg <kaspruk.oleg2002@gmail.com>
2025-02-04 15:39:37 -0600

    Added NotFound Page

---
+++ client/src/components/routing/AllRoutes.js
@@ -0,0 +1,96 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Register from '../auth/Register';
+import Login from '../auth/Login';
+import Alert from '../layout/Alert';
+import Dashboard from '../dashboard/Dashboard';
+import CreateProfile from '../profile-forms/CreateProfile';
+import EditProfile from '../profile-forms/EditProfile';
+import AddExperience from '../profile-forms/AddExperience';
+import AddEducation from '../profile-forms/AddEducation';
+import Profiles from '../profiles/Profiles';
+import Profile from '../profile/Profile';
+import Posts from '../posts/Posts';
+import Post from '../post/Post';
+import NotFound from '../layout/NotFound';
+import PrivateRoute from '../routing/PrivateRoute';
+
+const AllRoutes = () => {
+  return (
+    <section className="container">
+            <Alert />
+            <Routes>
+              <Route path="/register" element={<Register />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/profiles" element={<Profiles />} />
+              <Route path="/profile/:id" element={<Profile />} />
+              <Route
+                path="/dashboard"
+                element={
+                  <PrivateRoute>
+                    {' '}
+                    <Dashboard />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path="/create-profile"
+                element={
+                  <PrivateRoute>
+                    {' '}
+                    <CreateProfile />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path="/edit-profile"
+                element={
+                  <PrivateRoute>
+                    {' '}
+                    <EditProfile />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path="/add-experience"
+                element={
+                  <PrivateRoute>
+                    {' '}
+                    <AddExperience />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path="/add-education"
+                element={
+                  <PrivateRoute>
+                    {' '}
+                    <AddEducation />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path="/posts"
+                element={
+                  <PrivateRoute>
+                    {' '}
+                    <Posts />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                path="/posts/:id"
+                element={
+                  <PrivateRoute>
+                    {' '}
+                    <Post />
+                  </PrivateRoute>
+                }
+              />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </section>
+  )
+}
+
+export default AllRoutes
--- client/src/store.js
+++ client/src/store.js
@@ -1,12 +1,12 @@
-import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { thunk } from 'redux-thunk';
 import rootReducer from './reducers';
 
 const initialState = {};
-
 const middleware = [thunk];
 
-const store = createStore(rootReducer, initialState, composeWithDevTools(applyMiddleware(...middleware)));
+const composeEnhancers = compose;
+
+const store = createStore(rootReducer, initialState, composeEnhancers(applyMiddleware(...middleware)));
 
 export default store;
